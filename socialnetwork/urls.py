b5af6bed5e9e924714797d34c919be974139cc98
Ruairi Ballantine <ruairiballantine@gmail.com>
2022-01-14 09:37:29 -0500

    added static path in urls.py v2

--- socialnetwork/urls.py
+++ socialnetwork/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('social/', include('social.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
